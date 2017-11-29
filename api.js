28ecdda98bbe326997d68024f6f574ad78ff1e21
Joao Moreno <mail@joaomoreno.com>
2017-11-29 09:44:43 +0100

    fix api

--- api.js
+++ api.js
@@ -1,15 +1,15 @@
 const spdlog = require('.');
 
-const console = new spdlog.StdoutColorMt("console");
+const console = new spdlog.StdoutColor("console");
 console.info("Welcome to spdlog!");
 console.info("An info message example {}..", 1);
 
-const basic = new spdlog.BasicLoggerMt("basic_logger", "logs/basic.txt");
+const basic = new spdlog.BasicLogger("basic_logger", "logs/basic.txt");
 
 // turn on async
 spdlog.setAsyncMode(8192);
 
 
-const fileLogger = new spdlog.RotatingLoggerMt("file_logger", "myfilename", 1024 * 1024 * 5, 3);
+const fileLogger = new spdlog.RotatingLogger("file_logger", "myfilename", 1024 * 1024 * 5, 3);
 
 fileLogger.info("Welcome to spdlog!");
