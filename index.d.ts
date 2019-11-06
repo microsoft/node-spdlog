c036ac582ad44111b349c7dab27e7c5af6c0f2f1
Joao Moreno <joao.moreno@microsoft.com>
2019-11-06 09:11:45 +0100

    add copyright

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,9 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for
+ *  license information.
+ *--------------------------------------------------------------------------------------------*/
+
 export const version: string;
 export function setAsyncMode(bufferSize: number, flushInterval: number): void;
 export function createRotatingLogger(name: string, filename: string, filesize: number, filecount: number): RotatingLogger;
