39293e28076ac85bd065fd07e3ac876ddfb3c73f
Black-Hole1 <158blackhole@gmail.com>
2022-11-30 21:43:33 +0800

    chore: upgrade mkdirp to ^1.0.4

--- index.js
+++ index.js
@@ -15,17 +15,10 @@ function createAsyncRotatingLogger(name, filepath, maxFileSize, maxFiles) {
 	return createLogger('rotating_async', name, filepath, maxFileSize, maxFiles);
 }
 
-function createLogger(loggerType, name, filepath, maxFileSize, maxFiles) {
-	return new Promise((c, e) => {
-		const dirname = path.dirname(filepath);
-		mkdirp(dirname, err => {
-			if (err) {
-				e(err);
-			} else {
-				c(new spdlog.Logger(loggerType, name, filepath, maxFileSize, maxFiles));
-			}
-		});
-	});
+async function createLogger(loggerType, name, filepath, maxFileSize, maxFiles) {
+	const dirname = path.dirname(filepath);
+	await mkdirp(dirname);
+	return new spdlog.Logger(loggerType, name, filepath, maxFileSize, maxFiles);
 }
 
 exports.createRotatingLogger = createRotatingLogger;
